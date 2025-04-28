675cd9a4b78f6ddeda1783360230e9bf02357c94
sai <saisreenath.mandaloju@gmail.com>
2025-04-28 15:06:16 +0000

    added faq item

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,1 +1,25 @@
-// Write your code here.
+import {Component} from 'react'
+
+import './index.css'
+
+class FaqItem extends Component {
+  render() {
+    const {item} = this.props
+    const {questionText, answerText} = item
+    const buttonText = console.log(item)
+    return (
+      <div className="faq-item">
+        <div className="question-container">
+          <h1 className="faq-question">{questionText}</h1>
+          <button type="button" className="clicked">
+            {buttonText}
+          </button>
+        </div>
+
+        <p className="faq-answer">{answerText}</p>
+      </div>
+    )
+  }
+}
+
+export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,1 +1,21 @@
-// Write your code here.
+import {Component} from 'react'
+
+import FaqItem from '../FaqItem'
+
+import './index.css'
+
+class Faqs extends Component {
+  render() {
+    const {faqsList} = this.props
+    console.log(faqsList)
+    return (
+      <div className="bg-container">
+        <div className="faq-container">
+          <h1 className="main-heading">FAQs</h1>
+          <FaqItem item={faqsList[0]} />
+        </div>
+      </div>
+    )
+  }
+}
+export default Faqs
