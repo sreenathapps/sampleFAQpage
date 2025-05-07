4e78565283010fd4cb7e268e7cd3281ba48b13e1
sai <saisreenath.mandaloju@gmail.com>
2025-05-07 06:27:08 +0000

    final commit

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,25 +1,39 @@
-import {Component} from 'react'
-
+// Write your code here.
 import './index.css'
 
-class FaqItem extends Component {
-  render() {
-    const {item} = this.props
-    const {questionText, answerText} = item
-    const buttonText = console.log(item)
-    return (
-      <div className="faq-item">
-        <div className="question-container">
-          <h1 className="faq-question">{questionText}</h1>
-          <button type="button" className="clicked">
-            {buttonText}
-          </button>
-        </div>
-
-        <p className="faq-answer">{answerText}</p>
-      </div>
-    )
+const FaqItem = props => {
+  const {eachQuestion, selectedAction} = props
+  const {id, answerText, questionText, isAnswerShown} = eachQuestion
+  const onIconClick = () => {
+    selectedAction(id)
   }
+  const result =
+    isAnswerShown === undefined || isAnswerShown === false ? 'Hide' : `Show`
+  const openIcon =
+    result === 'Hide'
+      ? 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+  const showAnswer = result === 'Show'
+  const altImageText = result === 'Hide' ? 'plus' : 'minus'
+  return (
+    <li className="eachQueryContainer">
+      <div className="shownContent">
+        <h1 className="eachQuestionHead">{questionText}</h1>
+        <img
+          alt={altImageText}
+          onClick={onIconClick}
+          className="iconImages"
+          src={openIcon}
+        />
+      </div>
+      {showAnswer && (
+        <div>
+          <hr />
+          <p className="answer-text">{answerText}</p>
+        </div>
+      )}
+    </li>
+  )
 }
 
 export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,21 +1,52 @@
+// Write your code here.
 import {Component} from 'react'
 
-import FaqItem from '../FaqItem'
+import FaqItem from '../FaqItem/index'
 
 import './index.css'
 
 class Faqs extends Component {
-  render() {
+  constructor(props) {
+    super(props)
     const {faqsList} = this.props
-    console.log(faqsList)
+    this.state = {
+      queryList: faqsList,
+    }
+  }
+
+  selectedContent = id => {
+    this.setState(prevState => ({
+      queryList: prevState.queryList.map(eachQuery => {
+        if (id === eachQuery.id) {
+          if (eachQuery.isAnswerShown === undefined) {
+            return {...eachQuery, isAnswerShown: true}
+          }
+          return {...eachQuery, isAnswerShown: !eachQuery.isAnswerShown}
+        }
+        return eachQuery
+      }),
+    }))
+  }
+
+  render() {
+    const {queryList} = this.state
     return (
-      <div className="bg-container">
-        <div className="faq-container">
-          <h1 className="main-heading">FAQs</h1>
-          <FaqItem item={faqsList[0]} />
+      <div className="container">
+        <div className="cardContainer">
+          <h1 className="page-head">FAQs</h1>
+          <ul>
+            {queryList.map(eachQuery => (
+              <FaqItem
+                key={eachQuery.id}
+                eachQuestion={eachQuery}
+                selectedAction={this.selectedContent}
+              />
+            ))}
+          </ul>
         </div>
       </div>
     )
   }
 }
+
 export default Faqs
